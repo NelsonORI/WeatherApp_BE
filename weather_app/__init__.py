f3dba7e85de361d9769cf9222ddf123904bfe286
Nelson Orina <orinamecha2@gmail.com>
2025-04-04 15:39:45 +0300

    Connected to postgress

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +8,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///weather.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('WEATHER_DB')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
